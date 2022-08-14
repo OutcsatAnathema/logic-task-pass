d6da6ff49dca083e6b612805c9dafb5b4559dd29
Noor-AlHuda <108547100+OutcsatAnathema@users.noreply.github.com>
2022-08-14 22:55:18 +0300

    Update Q3.py

--- logic-pass/Q3.py
+++ logic-pass/Q3.py
@@ -1,19 +1,14 @@
-class String:
-    def __init__(self):
-        pass
+def occurrence():
 
-    def occurrence(self):
+    s = input("Enter your sentence: ")
+    fs = input("Enter a letter: ")
 
-        s = input("Enter your sentence: ")
-        fs = input("Enter a letter: ")
+    count = 0
+    for i in s:
+        if i == fs:
+            count += 1
 
-        count = 0
-        for i in s:
-            if i == fs:
-                count += 1
-
-        print(fs, " is occurred ", count, " times.")
+    print(fs, " is occurred ", count, " times.")
 
 
-s = String()
-s.occurrence()
+occurrence()
